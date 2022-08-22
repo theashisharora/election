4889617d4d52261b9c4f8a49d256e631fc898f4c
steph-crown <emmanuelstephen024@gmail.com>
2022-08-22 11:38:32 +0100

    Complete features section

--- src/components/ChangeFeaturesCard/ChangeFeaturesCard.jsx
+++ src/components/ChangeFeaturesCard/ChangeFeaturesCard.jsx
@@ -1,5 +1,6 @@
 // Imports
 import React from "react";
+import { Link } from "react-router-dom";
 import { ChangeFeaturesCardWrapper } from "./styles";
 
 const ChangeFeaturesCard = ({
@@ -12,6 +13,17 @@ const ChangeFeaturesCard = ({
   return (
     <ChangeFeaturesCardWrapper>
       <h3 className="card__header">{title}</h3>
+      <h3 className="card__header card__header--animate">{title}</h3>
+      <p className="card__description">{description}</p>
+      {isExternal ? (
+        <a href={link} className="card__cta">
+          {ctaLabel}
+        </a>
+      ) : (
+        <Link to={link} className="card__cta">
+          {ctaLabel}
+        </Link>
+      )}
     </ChangeFeaturesCardWrapper>
   );
 };
--- src/components/ChangeFeaturesCard/styles.js
+++ src/components/ChangeFeaturesCard/styles.js
@@ -11,16 +11,23 @@ export const ChangeFeaturesCardWrapper = styled.article`
   justify-content: flex-end;
   align-items: flex-start;
   color: white;
+  overflow-y: hidden;
+
+  * {
+    transition: 0.8s;
+  }
 
   min-height: 500px;
   @media screen and (min-width: 640px) {
     min-height: 500px;
+    padding: 32px 24px;
   }
   @media screen and (min-width: 800px) {
     min-height: 500px;
   }
   @media screen and (min-width: 960px) {
     min-height: 500px;
+    padding: 32px;
   }
   @media screen and (min-width: 1024px) {
     min-height: 500px;
@@ -34,6 +41,7 @@ export const ChangeFeaturesCardWrapper = styled.article`
       font-weight: 600;
       line-height: 130%;
       text-align: left;
+      /* transform: translateY(100%); */
 
       font-size: 2.6rem;
       @media screen and (min-width: 640px) {
@@ -51,6 +59,92 @@ export const ChangeFeaturesCardWrapper = styled.article`
       @media screen and (min-width: 1200px) {
         font-size: 3.2rem;
       }
+
+      &--animate {
+        display: none;
+        transform: translateY(100px);
+      }
+    }
+
+    &__description {
+      font-weight: 400;
+      font-size: 1.6rem;
+      line-height: 150%;
+      opacity: 0;
+      text-align: left;
+      margin-top: 24px;
+      display: none;
+      transform: translateY(100px);
+
+      font-size: 1.4rem;
+      @media screen and (min-width: 960px) {
+        font-size: 1.6rem;
+      }
+    }
+
+    &__cta {
+      margin-top: 40px;
+      padding: 16px 32px;
+      max-width: 200px;
+      max-height: 56px;
+      font-weight: 600;
+      line-height: 150%;
+      background: #116a4e;
+      border-radius: 8px;
+      font-size: 1.4rem;
+      width: 200px;
+      height: 56px;
+      max-width: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      @media screen and (min-width: 960px) {
+        font-size: 1.6rem;
+      }
+    }
+  }
+
+  &:hover {
+    .card {
+      &__description {
+        animation-name: slide-up-slowly;
+        animation-duration: 1s;
+        animation-fill-mode: forwards;
+        animation-timing-function: ease-in-out;
+        display: block;
+        transform: translateY(0);
+        opacity: 0.8;
+      }
+
+      &__header {
+        display: none;
+        &--animate {
+          animation-name: slide-up-slowly;
+          animation-duration: 1s;
+          animation-fill-mode: forwards;
+          animation-timing-function: ease-in-out;
+          display: block;
+          transform: translateY(0);
+          opacity: 1;
+        }
+      }
+
+      &__cta {
+        background-color: white;
+        color: #0b4734;
+      }
+    }
+  }
+
+  @keyframes slide-up-slowly {
+    0% {
+      transform: translateY(100px);
+      opacity: 0;
+    }
+    100% {
+      transform: translateY(0);
+      opacity: 0.8;
     }
   }
 `;
