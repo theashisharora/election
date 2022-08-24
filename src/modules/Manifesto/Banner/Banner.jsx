d780c085bf65a4ac4fa1ea65fa3c0d67bf05c249
steph-crown <emmanuelstephen024@gmail.com>
2022-08-24 10:05:35 +0100

    Complete banner section of manifesto page

--- src/modules/Manifesto/Banner/Banner.jsx
+++ src/modules/Manifesto/Banner/Banner.jsx
@@ -7,16 +7,38 @@ const Banner = () => {
   return (
     <Wrapper className="container">
       <section className="container__sub subb">
-        {/* <div className="banner__left"> */}
         <h2 data-aos="fade-up">CHANGE THE SENATE </h2>
         <h1 data-aos="fade-up">CHANGE NIGERIA</h1>
 
-        {/* </div> */}
         <img
           src={manifestoBannerImage}
-          className="banner__right"
+          className="banner__image"
           alt="Oladeji Blessing in a senatorial pose"
         />
+
+        <div className="mission">
+          <h3 className="mission__header">Mission & Policy</h3>
+          <article className="mission__body">
+            <p className="mission__body--text">
+              It is the job of the Senate to make legislation that secure, unite
+              and make Nigeria productive. This is a long-term project that
+              requires consistency. We can not achieve much of that with the
+              people who do not care or with those who secured their candidature
+              with a N100m application form
+            </p>
+            <p className="mission__body--text">
+              My mission is to change Nigeria and that will require changing
+              those that represent us in the upper chambers of the National
+              Assembly. We cannot do the same thing and get a different result.
+              We cannot elect the same crop of politicians and change the
+              country.
+            </p>
+            <p className="mission__body--text">
+              So, I have the honour today to present myself to you as the next
+              senator of Lagos Central Senatorial district – God willing!
+            </p>
+          </article>
+        </div>
       </section>
     </Wrapper>
   );
@@ -31,25 +53,35 @@ const Wrapper = styled.section`
 
   .container {
     &__sub {
-      padding-top: 80px;
+      padding: 80px 0;
 
       @media screen and (min-width: 960px) {
-        padding-top: 40px;
-        flex-wrap: nowrap;
+        padding: 100px 0;
+      }
+      @media screen and (min-width: 1024px) {
+        padding: 120px 0;
+      }
+      @media screen and (min-width: 1200px) {
+        padding: 140px 0;
       }
     }
   }
 
+  /* Mission */
   .banner {
-    /* &__left {
+    &__image {
       width: 100%;
-    } */
-    &__right {
-      width: 100%;
-
+      margin-top: 30px;
+      @media screen and (min-width: 720px) {
+        margin-top: 40px;
+      }
       @media screen and (min-width: 960px) {
-        width: 542px;
+        width: 390px;
         max-width: 40%;
+        margin-top: 50px;
+      }
+      @media screen and (min-width: 1024px) {
+        margin-top: 60px;
       }
     }
   }
@@ -98,34 +130,66 @@ const Wrapper = styled.section`
     }
   }
 
-  p {
-    font-weight: 800;
-    font-size: 1.6rem;
-    line-height: 133%;
-    color: #40423a;
-    margin-top: 20px;
+  .mission {
+    width: 100%;
+    max-width: 670px;
+    margin: 0 auto;
 
-    @media screen and (min-width: 640px) {
-      font-size: 1.8rem;
-    }
-    @media screen and (min-width: 800px) {
-      font-size: 2rem;
+    margin-top: 30px;
+    @media screen and (min-width: 720px) {
+      margin-top: 40px;
     }
     @media screen and (min-width: 960px) {
-      font-size: 2.2rem;
+      margin-top: 50px;
     }
-    @media screen and (min-width: 1200px) {
-      font-size: 2.4rem;
+    @media screen and (min-width: 1024px) {
+      margin-top: 60px;
     }
-  }
 
-  .btn__filled {
-    margin-top: 20px;
-    width: 185px;
-    max-width: 100%;
+    &__header {
+      font-weight: 600;
+      font-size: 48px;
+      line-height: 116%;
+      text-align: center;
+      color: #171d17;
+      font-size: 2.8rem;
+      margin-bottom: 20px;
+      @media screen and (min-width: 640px) {
+        font-size: 3.2rem;
+      }
+      @media screen and (min-width: 800px) {
+        font-size: 3.6rem;
+        margin-bottom: 30px;
+      }
+      @media screen and (min-width: 960px) {
+        font-size: 4rem;
+      }
+      @media screen and (min-width: 1024px) {
+        font-size: 4.4rem;
+        margin-bottom: 40px;
+      }
+      @media screen and (min-width: 1200px) {
+        font-size: 4.8rem;
+      }
+    }
 
-    @media screen and (min-width: 960px) {
-      padding: 16px 32px;
+    &__body {
+      &--text {
+        font-weight: 600;
+        line-height: 150%;
+        text-align: center;
+        color: #747774;
+        font-size: 1.4rem;
+        margin-bottom: 14px;
+        @media screen and (min-width: 960px) {
+          font-size: 1.6rem;
+          margin-bottom: 16px;
+        }
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+      }
     }
   }
 `;
